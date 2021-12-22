f4131eacbf17ee3b435ed1508df7a49f31d90d04
IngoStramm <ingostramm@gmail.com>
2021-12-22 16:19:49 -0300

    remove version from css

--- functions.php
+++ functions.php
@@ -95,3 +95,13 @@ function ipf_remove_required_fields($required_fields)
 
     return $required_fields;
 }
+
+// remove wp version number from scripts and styles
+function remove_css_js_version($src)
+{
+    if (strpos($src, '?ver='))
+        $src = remove_query_arg('ver', $src);
+    return $src;
+}
+add_filter('style_loader_src', 'remove_css_js_version', 9999);
+// add_filter('script_loader_src', 'remove_css_js_version', 9999);
