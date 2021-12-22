123f704c59ea77bd01624305af98b11da01c1c04
IngoStramm <ingostramm@gmail.com>
2021-12-22 16:42:25 -0300

    remove version from js files

--- functions.php
+++ functions.php
@@ -104,4 +104,4 @@ function remove_css_js_version($src)
     return $src;
 }
 add_filter('style_loader_src', 'remove_css_js_version', 9999);
-// add_filter('script_loader_src', 'remove_css_js_version', 9999);
+add_filter('script_loader_src', 'remove_css_js_version', 9999);
