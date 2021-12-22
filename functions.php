92968aa94c4f456e5771b741a51e95b39cb3273b
IngoStramm <ingostramm@gmail.com>
2021-12-22 16:05:47 -0300

    remove display name and other fields from account

--- functions.php
+++ functions.php
@@ -48,3 +48,50 @@ function ipf_wc_redirect_to_shop_page($redirect, $user)
     else
         return $shop_page_url;
 }
+
+// Remove os campos do checkout
+
+add_filter('woocommerce_checkout_fields', 'ipf_remove_checkout_fields');
+
+function ipf_remove_checkout_fields($fields)
+{
+
+    // ipf_debug($fields);
+
+    // billing
+    unset($fields['billing']['billing_company']);
+    unset($fields['billing']['billing_country']);
+    unset($fields['billing']['billing_address_1']);
+    unset($fields['billing']['billing_address_2']);
+    unset($fields['billing']['billing_city']);
+    unset($fields['billing']['billing_state']);
+    unset($fields['billing']['billing_postcode']);
+    unset($fields['billing']['billing_phone']);
+
+    // shipping
+    unset($fields['shipping']['shipping_company']);
+    unset($fields['shipping']['shipping_country']);
+    unset($fields['shipping']['shipping_address_1']);
+    unset($fields['shipping']['shipping_address_2']);
+    unset($fields['shipping']['shipping_city']);
+    unset($fields['shipping']['shipping_state']);
+    unset($fields['shipping']['shipping_postcode']);
+
+    // order_comments
+    unset($fields['order']['order_comments']);
+
+    return $fields;
+}
+
+// Remove o titulo "Informação Adicional" que vem após os campos do checkout
+add_filter('woocommerce_enable_order_notes_field', '__return_false');
+
+// Remove o campo "display name" dos detalhes da conta
+add_filter('woocommerce_save_account_details_required_fields', 'ipf_remove_required_fields');
+
+function ipf_remove_required_fields($required_fields)
+{
+    unset($required_fields['account_display_name']);
+
+    return $required_fields;
+}
