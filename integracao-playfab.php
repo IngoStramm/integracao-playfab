f4853796de91621662abf397f84a2dba46154240
IngoStramm <ingostramm@gmail.com>
2022-04-13 14:19:30 -0300

    new version

--- integracao-playfab.php
+++ integracao-playfab.php
@@ -4,7 +4,7 @@
  * Plugin Name: Integração Playfab
  * Plugin URI: https://agencialaf.com
  * Description: Descrição do Integração Playfab.
- * Version: 0.2.16
+ * Version: 0.2.17
  * Author: Ingo Stramm
  * Text Domain: integracao-playfab
  * License: GPLv2
